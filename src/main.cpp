d530b5874857f9bac93cb48f6826f98d004a9eec
Daniel Lopes <danielorkae@gmail.com>
2023-06-14 10:15:11 -0300

    chore: add main

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include "entities/empresa.hpp"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    // Criar uma instância da classe Empresa dinamicamente
+    Empresa *empresa = new Empresa();
+
+    // Chamar a função carregarDados()
+    empresa->carregarDados("dados_empresa.txt");
+
+    // Resto do código...
+
+    // Liberar a memória alocada pela instância da classe Empresa
+    delete empresa;
+
     return 0;
 }
