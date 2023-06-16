c511db6dc09bae4274af4477a586ea8620fb9f6f
Daniel Lopes <danielorkae@gmail.com>
2023-06-16 00:21:55 -0300

    feat: adiciona calculo de salario e recisão para asg

--- src/entities/asg.cpp
+++ src/entities/asg.cpp
@@ -6,7 +6,7 @@ Asg::Asg() : Pessoa(), Funcionario(), adicionalInsalubridade(0.0)
 }
 
 Asg::Asg(std::string nome, std::string cpf, Data dataNascimento, Endereco enderecoPessoal,
-         std::string estadoCivil, int qtdFilhos, std::string salario, std::string matricula,
+         std::string estadoCivil, int qtdFilhos, float salario, std::string matricula,
          Data ingressoEmpresa, float adicionalInsalubridade)
     : Pessoa(nome, cpf, dataNascimento, enderecoPessoal, estadoCivil, qtdFilhos),
       Funcionario(salario, matricula, ingressoEmpresa),
@@ -25,21 +25,35 @@ void Asg::setAdicionalInsalubridade(float adicionalInsalubridade)
     this->adicionalInsalubridade = adicionalInsalubridade;
 }
 
-double Asg::calcularSalario(int diasFaltas)
+float Asg::calcularSalario(int diasFaltas)
 {
-    // Cálculo do salário do ASG considerando os dias de faltas
-    double salarioBase = std::stod(getSalario());
-    double salarioDia = salarioBase / 30.0;
-    double descontoFaltas = salarioDia * diasFaltas;
-    double salarioFinal = salarioBase - descontoFaltas + adicionalInsalubridade;
-
+    // Cálculo do salário do ASG considerando os dias de faltas, adicional de insalubridade e adicional por filho
+    float salarioBase = getSalario();
+    float salarioDescontado = salarioBase - (salarioBase / 30) * diasFaltas;
+    float salarioFinal = salarioDescontado + adicionalInsalubridade + (getQtdFilhos() * 100.0f);
     return salarioFinal;
 }
 
-double Asg::calcularRecisao(Data desligamento)
+float Asg::calcularRecisao(Data desligamento)
 {
-    // Cálculo da rescisão do ASG com base na data de desligamento
-    // TODO: Ver cálculo da rescisão do ASG
+    Data dataIngresso = getIngressoEmpresa();
 
-    return 0.0;
+    // Converter a data de ingresso para fração de ano
+    float fracaoAnoIngresso = static_cast<float>(dataIngresso.dia) / 365.0f;
+    fracaoAnoIngresso += static_cast<float>(dataIngresso.mes) / 12.0f;
+    fracaoAnoIngresso += static_cast<float>(dataIngresso.ano);
+
+    // Converter a data de desligamento para fração de ano
+    float fracaoAnoDesligamento = static_cast<float>(desligamento.dia) / 365.0f;
+    fracaoAnoDesligamento += static_cast<float>(desligamento.mes) / 12.0f;
+    fracaoAnoDesligamento += static_cast<float>(desligamento.ano);
+
+    // Calcular o tempo de trabalho em anos
+    float tempoTrabalho = fracaoAnoDesligamento - fracaoAnoIngresso;
+
+    // Calcular a rescisão considerando o salário base por ano trabalhado
+    float salarioBaseAnual = getSalario();
+    float salarioRecisao = salarioBaseAnual * tempoTrabalho;
+
+    return salarioRecisao;
 }
