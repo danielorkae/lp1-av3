d90cd50a20044a2f6f61d950e42e1ac93ffc41d7
Daniel Lopes <danielorkae@gmail.com>
2023-07-05 01:39:50 -0300

    doc: add comments to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include "entities/empresa.hpp"
 
+/**
+ * @brief O projeto é uma aplicação de gerenciamento de funcionários
+ * em uma empresa. Permite cadastrar e manipular informações de diferentes tipos
+ * de funcionários, como vendedores, gerentes e auxiliares de serviços gerais
+ * (ASGs). Além disso, possibilita salvar os dados dos funcionários em arquivos
+ * e realizar operações como cálculo de salários e busca por informações
+ * específicas.
+ * 
+ * @author Daniel Lopes
+ * @date 2023-07-04
+ *
+ * @return int
+ */
 int main()
 {
     // Criar uma instância da classe Empresa dinamicamente
